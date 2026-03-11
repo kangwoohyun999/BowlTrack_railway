36093f505d51953c20026b8017c0e28b6a58ae4a
wh1012 <63694003+kangwoohyun999@users.noreply.github.com>
2026-03-11 16:21:28 +0900

    scroll, server

--- app.py
+++ app.py
@@ -180,4 +180,6 @@ def update_settings():
     return jsonify({'ok': True})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
